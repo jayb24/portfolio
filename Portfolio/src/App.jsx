33eaa4c44bb6f24276ef613f23f50866abc29721
Jayden Botha <jaybotha05@gmail.com>
2025-03-08 19:54:55 -0500

    added components

--- Portfolio/src/App.jsx
+++ Portfolio/src/App.jsx
@@ -1,8 +1,27 @@
-import React from 'react'
+import styles from "./style";
+import { Navbar, Hero, TechStack, Projects } from "./components";
 
 const App = () => (
-  <div className= "bg-primary text-center">
-    <h1>Hello, World</h1>
+  <div className= "bg-primary w-full overflow-hidden">
+    <div className={`${styles.paddingX} ${styles.flexCenter}`}>
+      <div className={`${styles.boxWidth}`}>
+        <Navbar />
+      </div>
+    </div>
+
+    <div className={`bg-primary ${styles.flexStart}`}>
+      <div className={`${styles.boxWidth}`}>
+        Hero
+      </div>
+    </div>
+
+    <div className={`bg-primary ${styles.paddingX} ${styles.flexStart}`}>
+      <div className={`${styles.boxWidth}`}>
+        <TechStack />
+        <Projects />
+      </div>
+    </div>
+
   </div>
 );
 
---
+++ Portfolio/src/components/index.js
@@ -0,0 +1,11 @@
+import Navbar from "./Navbar";
+import Hero from "./Hero";
+import TechStack from "./TechStack";
+import Projects from "./Projects";
+
+export {
+    Navbar, 
+    Hero,
+    TechStack,
+    Projects
+};
