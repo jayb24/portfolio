b9ed1197728001af6f39606a963b45453728eb5b
Jayden Botha <jaybotha05@gmail.com>
2025-03-10 18:42:21 -0400

    Added hero section

--- Portfolio/src/App.jsx
+++ Portfolio/src/App.jsx
@@ -2,7 +2,7 @@ import styles from "./style";
 import { Navbar, Hero, TechStack, Projects } from "./components";
 
 const App = () => (
-  <div className= "bg-primary w-full overflow-hidden">
+  <div className= "bg-black w-full overflow-hidden">
     <div className={`${styles.paddingX} ${styles.flexCenter}`}>
       <div className={`${styles.boxWidth}`}>
         <Navbar />
--- Portfolio/src/components/Navbar.jsx
+++ Portfolio/src/components/Navbar.jsx
@@ -34,7 +34,7 @@ const Navbar = () => {
 
                     {/* Mobile Menu */}
                     <div
-                        className={`${toggle ? 'flex' : 'hidden'} p-6 bg-black-gradient absolute top-20 right-0 mx-4 my-2 min-w-[140px] rounded-xl sidebar`}
+                        className={`${toggle ? 'flex' : 'hidden'} p-6  absolute top-20 right-0 mx-4 my-2 min-w-[140px] rounded-xl sidebar`}
                     >
                         <ul className="list-none flex flex-col justify-end items-center flex-1">
                             {navLinks.map((nav, index) => (
