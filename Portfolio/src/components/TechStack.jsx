06725c17de0d42e3cdd9779d666f38c243bd5604
Jayden Botha <jaybotha05@gmail.com>
2025-03-13 17:18:03 -0400

    Finalized tech stack

--- Portfolio/src/components/TechStack.jsx
+++ Portfolio/src/components/TechStack.jsx
@@ -1,19 +1,22 @@
 import { techStack } from '../constants';
 
 const TechStack = () => (
-<div className="w-full justify-center mx-auto px-8" id="tech-stack">
-      <h2 className="text-2xl text-white font-medium mb-4 text-left sm:text-center xs:text-center md:text-left">My Tech Stack</h2>
-      <div className="w-full h-0.5 bg-white mb-4"></div>
-      
-      <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-6 md:gap-8">
-        {techStack.map((tech, index) => (
-          <div key={index} className="flex flex-col items-center justify-center p-1 hover:bg-gray-50 rounded-lg transition-colors">
-            <img src={tech.icon} alt={tech.name} className="w-30 h-30 mb-2" />
-            <span className="text-sm text-white text-center">{tech.name}</span>
-          </div>
-        ))}
-      </div>
+  <div className="w-full justify-center mx-auto px-8" id="tech-stack">
+    <h2 className="text-2xl text-white font-medium mb-1 text-left sm:text-center xs:text-center md:text-left">My Tech Stack</h2>
+    <div className="w-full h-0.5 bg-white mb-4"></div>
+    
+    <div className="grid grid-cols-3 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-4 justify-between">
+      {techStack.map((tech, index) => (
+        <div 
+          key={index} 
+          className="flex items-center p-2 hover:bg-white border-white border-2 rounded-lg transition-colors"
+        >
+          <img src={tech.icon} alt={tech.name} className="w-6 h-6 mr-2" />
+          <span className="text-sm text-white">{tech.name}</span>
+        </div>
+      ))}
     </div>
+  </div>
 );
 
 export default TechStack;
--- Portfolio/src/constants/index.js
+++ Portfolio/src/constants/index.js
@@ -23,7 +23,7 @@ export const techStack = [
     { name: 'Java', icon: java },
     { name: 'JavaScript', icon: javaScript },
     { name: 'TypeScript', icon: typescript },
-    { name: 'C', icon: c },
+    { name: 'C/C++', icon: c },
     { name: 'HTML', icon: html },
     { name: 'CSS', icon: css },
     { name: 'Python', icon: python },
