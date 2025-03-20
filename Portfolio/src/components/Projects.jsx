a4b892cceddefbc34f4ae0476d98ff724260420d
Jayden Botha <jaybotha05@gmail.com>
2025-03-19 20:57:16 -0400

    added projects

--- Portfolio/src/components/Projects.jsx
+++ Portfolio/src/components/Projects.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 
-const Projects = () => {
-  return (
+const Projects = () => (
+  <div className="w-full justify-center mx-auto px-8" id="Projects" >
     <div>
-      
+      <h2 className="text-2xl text-white font-medium mb-1 text-left sm:text-center xs:text-center md:text-left">Projects</h2>
+      <div className="w-full h-0.5 bg-white mb-3"></div>
     </div>
-  )
-}
+      
+  </div>
+)
 
 export default Projects
